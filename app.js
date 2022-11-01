65b34974eb61a9f41564b108c886661774631c06
André Moura <andrebessadmoura@gmail.com>
2022-10-31 23:07:54 -0300

    Projeto
    Projeto prog web

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bootstrap-5.2.2-dist')));
 
 app.use('/', indexRouter);
 app.use('/produtos', produtosRouter);
